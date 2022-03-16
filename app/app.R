970a717e2359dea0e71176b54441f2ee4bad299a
Vanessa Martínez Romero <35711179+yosassenav@users.noreply.github.com>
2022-03-16 15:17:38 -0500

    main layout first attempt

--- app/app.R
+++ app/app.R
@@ -3,20 +3,39 @@ library(shinydashboard)
 library(dplyr)
 library(ggplot2)
 
+header <- dashboardHeader(title = "Dashboard CIAM")
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Tablero de Canalización", icon = icon("dashboard"), startExpanded = TRUE, 
+      menuSubItem("Contacto", tabName = "contacto"),
+      menuSubItem("Canalización anterior", tabName = "canalizacion"),
+      menuSubItem("Calificaciones instancias", tabName = "calificaciones")),
+
+    menuItem("Registro de Violencia", icon = icon("bar-chart-o"), startExpanded = TRUE,
+      menuSubItem("Violencia experimentada", tabName = "violencia"),
+      menuSubItem("Tipos de violencia actuales", tabName = "vactuales"),
+      menuSubItem("Datos demográficos", tabName = "demograficos"),
+      menuSubItem("Datos del agresor", tabName = "agresor"))
+  )
+)
+   
+
 body <- dashboardBody(
-    tabBox(
-        title = "Sample tabBox",
-        id = "tabset1", height = "600px", width = "500px",
-        tabPanel("Tab1", "First Tab"),
-        tabPanel("Tab2", "Second Tab"),
-        ),
+   tabItems(
+     tabItem("contacto", h2("Medios de contacto")),
+     tabItem("violencia", h2("Registro de Violencia"))
+
+   )
   )
 
+
+
 ui <- dashboardPage(
-  header = dashboardHeader(),
-  sidebar = dashboardSidebar(),
-  body = body,
-  skin = "purple",
+  header,
+  sidebar,
+  body,
+  skin = "purple"
 )
 
 server <- function(input, output) { }
