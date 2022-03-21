3cd02209edd1fa6a8de3e29aa84f5771d4bb89e0
Vanessa Martínez Romero <35711179+yosassenav@users.noreply.github.com>
2022-03-20 20:45:28 -0500

    api call from shiny app first attempt

--- app/server.R
+++ app/server.R
@@ -1,5 +1,31 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(jsonlite)
+library(httr)
 
-server <- function(input, output, session) { }
+    
+
+server <- function(input, output, session) {
+    base_url <- "http://127.0.0.1:8080/api/"
+    infor_url <- "folio"
+    full_url <- base::paste0(base_url, infor_url)
+    api_call <- httr::GET(full_url) 
+  
+    #retrieving json files
+    folios_json <- jsonlite::fromJSON(full_url)
+
+    #personas_json <- jsonlite::fromJSON("http://http://127.0.0.1:8080/api/persona")
+
+
+    #retrieving api's response leaving the status out
+    folios <- folios_json$response
+
+    folios
+
+    #personas <- personas_json$response
+
+
+
+
+}
