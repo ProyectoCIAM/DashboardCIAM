f9313024997f66c6d55d3840c3bf122a36faaeb0
Michelle Villegas <180300350@ucaribe.edu.mx>
2022-03-23 22:15:50 -0500

    code fix d1 tab1

--- app/server.R
+++ app/server.R
@@ -8,47 +8,57 @@ library(httr)
     
 
 server <- function(input, output, session) {
+    # llamada tablas
+    base_url <- "http://127.0.0.1:8080/api/" # url raiz
 
-    ########################        DASHBOARD 1  PART 1
-    base_url <- "http://127.0.0.1:8080/api/"
-
-
-    ###     MEDIO DE CONTACTO    ###
-
+    # urls
     full_url_folio <- base::paste0(base_url, "folio")
-
+    full_url_medio_contacto <- base::paste0(base_url, "medio_contacto")
+    full_url_persona <- base::paste0(base_url, "persona")
+    full_url_como_se_entero_cat <- base::paste0(base_url, "como_se_entero")
+    full_url_canalizacion_seguimiento <- base::paste0(base_url, "canalizacion_seguimiento")
+    full_url_instancia <- base::paste0(base_url, "instancia")
 
     #api call
     api_call_folio <- httr::GET(full_url_folio)
-  
+    api_call_medio_contacto <- httr::GET(full_url_medio_contacto)
+    api_call_persona <- httr::GET(full_url_persona)
+    api_call_como_se_entero_cat <- httr::GET(full_url_como_se_entero_cat)
 
     #retrieving json file
     folios_json <- jsonlite::fromJSON(full_url_folio)
-
+    medio_contacto_json <- jsonlite::fromJSON(full_url_medio_contacto)
+    personas_json <- jsonlite::fromJSON(full_url_persona)
+    como_se_entero_catalogo_json <- jsonlite::fromJSON(full_url_como_se_entero_cat)
+    canalizacion_seguimiento_json <- jsonlite::fromJSON(full_url_canalizacion_seguimiento)
+    instancia_json <- jsonlite::fromJSON(full_url_instancia)
 
     #retrieving api's response leaving the status out
     folios <- folios_json$response
+    medio_contacto <- medio_contacto_json$response
+    personas <- personas_json$response
+    como_se_entero_catalogo <- como_se_entero_catalogo_json$response
+    canalizacion_seguimiento <- canalizacion_seguimiento_json$response
+    instancia <- instancia_json$response
 
 
-    #counting id's from folios' column id_contacto_catalogo
-    num_presencial <- sum(ifelse(folios$id_contacto_catalogo == "1", 1, 0))
-
-    num_telefonico <- sum(ifelse(folios$id_contacto_catalogo == "2", 1, 0))
-
-    num_redes_sociales <- sum(ifelse(folios$id_contacto_catalogo == "3", 1, 0))
-
-    num_otro <- sum(ifelse(folios$id_contacto_catalogo == "4", 1, 0))
 
 
-    folio_contacto <- data.frame(
-    category = c("Presencial","Telefónico","Redes Sociales","Otro"), 
-    value = c(num_presencial, num_presencial, num_redes_sociales, num_otro)
-    )
+    ########################        DASHBOARD 1  PART 1
+    ###     MEDIO DE CONTACTO    ###
 
+    # cambio de id para poder hacer merge con catalogo
+    colnames(folios)[3] <- "id_medio_contacto"
+    # solo por cambiar título de columna nombra a medio contacto
+    colnames(medio_contacto)[2] <- "medio_contacto"
+    # hacemos merge
+    foliosxmedioc <- merge(folios,medio_contacto)
+    # count por tipo de medio contacto
+    folio_contacto <- foliosxmedioc %>% count(medio_contacto)
 
     output$contacto <- renderPlot({
         # Compute percentages
-        folio_contacto$fraction <- folio_contacto$value / sum(folio_contacto$value)
+        folio_contacto$fraction <- folio_contacto$n / sum(folio_contacto$n)
 
         # Compute the cumulative percentages (top of each rectangle)
         folio_contacto$ymax <- cumsum(folio_contacto$fraction)
@@ -60,10 +70,10 @@ server <- function(input, output, session) {
         folio_contacto$labelPosition <- (folio_contacto$ymax + folio_contacto$ymin) / 2
 
         # Compute a good label
-        folio_contacto$label <- paste0(folio_contacto$category, "\n valor: ", folio_contacto$value)
+        folio_contacto$label <- paste0(folio_contacto$medio_contacto, "\n valor: ", folio_contacto$n)
 
         # Make the plot
-        ggplot(folio_contacto, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=category)) +
+        ggplot(folio_contacto, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=medio_contacto)) +
         geom_rect() +
         geom_label( x=3.5, aes(y=labelPosition, label=label), size=6) +
         scale_fill_brewer(palette=3) +
@@ -75,19 +85,12 @@ server <- function(input, output, session) {
 
 
     ###     ¿La persona que contacta es quien requiere el servicio?    ###
-
-
-    ## this question also uses folios table
-
     #counting id's from folios' column id_contacto_catalogo
 
-    persona_requiere_servicio_count <- sum(ifelse(folios$filtro_si == "1",1,0))  ### no esta regresando nada, pero debería regresar 19
+    persona_requiere_servicio_count <- sum(!is.na(folios$filtro_si))
+    persona_otra_requiere_servicio_count <- sum(!is.na(folios$filtro_no))
 
-    persona_otra_requiere_servicio_count <- sum(ifelse(folios$filtro_no == "1",1,0))  ### no esta regresando nada, pero debería regresar 17
-
-
-    persona_solicita_servicio <- data.frame(category = c("Si", "No"), value = c(19, 17))
-
+    persona_solicita_servicio <- data.frame(category = c("Si", "No"), value = c(persona_requiere_servicio_count, persona_otra_requiere_servicio_count))
 
     output$persona_servicio <- renderPlotly({ggplotly(
         ggplot(persona_solicita_servicio, aes(x=category, y=value, fill = category)) +
@@ -101,47 +104,21 @@ server <- function(input, output, session) {
 
 
     ###     ¿Cómo se enteró de los servicios de acompañamiento emocional?    ###
-
-    full_url_persona <- base::paste0(base_url, "persona")
-
-    #api call
-    api_call_persona <- httr::GET(full_url_persona)
-
-    #retrieving json file
-    personas_json <- jsonlite::fromJSON(full_url_persona)
-
-    #retrieving api's response leaving the status out
-    personas <- personas_json$response
-
-
     #counting ids from personas' column idComoSeEntero
-    cont_redes_sociales <- sum(ifelse(personas$idComoSeEntero == "1", 1, 0))
+    colnames(personas)[16] <- "id_como_se_entero"
+    colnames(como_se_entero_catalogo)[2] <- "como_se_entero"
 
-    cont_recomendacion_familiar <- sum(ifelse(personas$idComoSeEntero == "2", 1, 0))
-
-    cont_recomendacion_colab <- sum(ifelse(personas$idComoSeEntero == "3", 1, 0))
-
-    cont_instancia <- sum(ifelse(personas$idComoSeEntero == "4", 1, 0))
-
-    cont_asociacion <- sum(ifelse(personas$idComoSeEntero == "5", 1, 0))
-
-    cont_otro <- sum(ifelse(personas$idComoSeEntero == "6", 1, 0))
-
-
-
-    servicio_acomp_emocional_data <- data.frame(
-        category = c("Redes Sociales","Recomendación de algún familiar","Recomendación de colaborador CIAM","Instancia gubernamental","Asociación civil","Otra"),
-        value = c(cont_redes_sociales, cont_recomendacion_familiar, cont_recomendacion_colab, cont_instancia, cont_asociacion, cont_otro)
-    )
+    servicio_acomp_emocional_data <- merge(personas,como_se_entero_catalogo)
+    servicio_acomp_emocional_data <- servicio_acomp_emocional_data %>% count(como_se_entero)
 
 
     output$serv_acomp_emocional <- renderPlotly({
-       ggplotly(
-           ggplot(servicio_acomp_emocional_data, aes(x=category, y=value, fill = category)) + 
-            geom_bar(stat="identity", alpha=.6, width=.4) + 
-            theme(axis.text.x = element_text(angle = 60, hjust=1)) +
-            xlab("Categoría") +
-            ylab("Frecuencia"))
+    ggplotly(
+        ggplot(servicio_acomp_emocional_data, aes(x=como_se_entero, y=n, fill = como_se_entero)) + 
+        geom_bar(stat="identity", alpha=.6, width=.4) + 
+        theme(axis.text.x = element_text(angle = 60, hjust=1)) +
+        xlab("Categoría") +
+        ylab("Frecuencia"))
     })
 
 
@@ -175,16 +152,6 @@ server <- function(input, output, session) {
 
     ########################        DASHBOARD 1  PART 3
     # traemos la informacion
-    full_url <- base::paste0(base_url, "canalizacion_seguimiento")
-    canalizacion_seguimiento_json <- jsonlite::fromJSON(full_url)
-
-    full_url <- base::paste0(base_url, "instancia")
-    instancia_json <- jsonlite::fromJSON(full_url)
-
-    # separamos el dataframe
-    canalizacion_seguimiento <- canalizacion_seguimiento_json$response
-    instancia <- instancia_json$response
-
     output$calificacionesxinstancia <- renderPlotly({
         colnames(canalizacion_seguimiento)[17] <- "id_instancia"
         # seleccionamos las columnas de interes
